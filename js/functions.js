104e29fcbca62c656cc854f42bf38097ae480fa4
walterzsh <126636356+walterzsh@users.noreply.github.com>
2023-03-20 22:33:41 -0300

    First commit

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+document.getElementById("item0").onclick=function(){
+    document.getElementById("info_perfil_encab").innerHTML="Nombre:";
+    document.getElementById("info_perfil").innerHTML="Walter Z";
+};
+
+document.getElementById("item1").onclick=function(){
+    document.getElementById("info_perfil_encab").innerHTML="Fecha de Nacimiento:";
+    document.getElementById("info_perfil").innerHTML="02/04/1978";
+};
+
+document.getElementById("item2").onclick=function(){
+    document.getElementById("info_perfil_encab").innerHTML="Dirección:";
+    document.getElementById("info_perfil").innerHTML="Alguna Calle N° 123.<br>Localidad General, Argentina.";
+};
+
+document.getElementById("item3").onclick=function(){
+    document.getElementById("info_perfil_encab").innerHTML="Mail:";
+    document.getElementById("info_perfil").innerHTML="cv@miSitioWeb.net";
+};
+
+document.getElementById("item4").onclick=function(){
+    document.getElementById("info_perfil_encab").innerHTML="Teléfonos:";
+    document.getElementById("info_perfil").innerHTML="Fijo: 4123-4567<br>Móvil: 54-9-11-4987-6543";
+};
